65c47b01a581b83181e712dc7b2c80555133ce9b
wzk <15521143125@163.com>
2021-06-10 10:02:13 +0800

    7

--- src/leetCode/Solution17.java
+++ src/leetCode/Solution17.java
@@ -1,13 +1,56 @@
 package leetCode;
 
+
+/**
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+ *
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 输入：digits = "23"
+ * 输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * 输入：digits = ""
+ * 输出：[]
+ */
+
+
+import com.sun.deploy.util.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Solution17 {
     public static void main(String[] args){
-        List<String> l =  new Solution17().letterCombinations("23");
+        List<String> l =  new Solution17().letterCombinations2("23");
         l.forEach(System.out::println);
     }
+
+
+
+    public List<String> letterCombinations2(String digits) {
+        List<String> l = new ArrayList<>();
+        if (digits == null || digits.equals("")){
+            return l;
+        }
+        int len = digits.length();
+        String [] ss = new String[len];
+        for (int i = 0 ;i < len ; i++){
+            ss[i] = getABC(Integer.parseInt(digits.substring(i, i+1)));
+        }
+        letter(ss, 0, "", l);
+        return l;
+    }
+    public void letter(String [] ss, int i,String s ,List<String> l) {
+        if (i == ss.length){
+            l.add(s);
+            return;
+        }
+        for (int j = 0;j<ss[i].length();j++){
+            letter(ss, i+1, s+ss[i].charAt(j), l);
+        }
+    }
+
+
+
+
     public List<String> letterCombinations(String digits) {
         List<String> l = new ArrayList<>();
         if(digits.length() == 0){
