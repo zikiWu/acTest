c21967b9b2d973f0405e5966971b2638890f7396
wuzekun <wzk792676244#>
2024-06-21 16:17:29 +0800

    ttt

--- src/leetCode/Solution138.java
+++ src/leetCode/Solution138.java
@@ -1,86 +1,99 @@
 package leetCode;
 
+/**
+ * 138. 随机链表的复制
+ * 已解答
+ * 中等
+ * 相关标签
+ * 相关企业
+ * 提示
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ *
+ * 构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+ *
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+ *
+ * 返回复制链表的头节点。
+ *
+ * 用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ *
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ * 你的代码 只 接受原链表的头节点 head 作为传入参数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ *
+ */
 public class Solution138 {
-    public static void main(String[] args){
 
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
     }
+    public static void main(String[] args) {
+        Node h = new Node(5);
+        Node h2 = new Node(6);
+        Node h3 = new Node(8);
+        h.next = h2;
+        h2.next = h3;
+        h.random = h3;
+        new Solution138().copyRandomList(h);
+    }
+
     public Node copyRandomList(Node head) {
-
-        if (head == null) {
-            return null;
+        Node r = head;
+        while (r != null) {
+            Node next = r.next;
+            Node insertNode = new Node(r.val);
+            r.next = insertNode;
+            insertNode.next = next;
+            r = next;
         }
-
-        // Creating a new weaved list of original and copied nodes.
-        Node ptr = head;
-        while (ptr != null) {
-
-            Node newNode = new Node();
-            newNode.val = ptr.val;
-            newNode.next = ptr.next;
-            ptr.next = newNode;
-            ptr = newNode.next;
+        r = head;
+        while (r != null) {
+            Node newNode = r.next;
+            newNode.random = r.random != null? r.random.next : null;
+            r = newNode.next;
         }
-
-        ptr = head;
-        while (ptr != null) {
-            ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
-            ptr = ptr.next.next;
+        r = head;
+        Node r2 = head != null ? head.next : null;
+        while (r != null && r.next != null) {
+            Node newNode = r.next;
+            Node nextNode = newNode.next;
+            if (nextNode != null) {
+                newNode.next = nextNode.next;
+            }
+            r.next = nextNode;
+            r = nextNode;
         }
-        Node ptr_old_list = head; // A->B->C
-        Node ptr_new_list = head.next; // A'->B'->C'
-        Node head_old = head.next;
-        while (ptr_old_list != null) {
-            ptr_old_list.next = ptr_old_list.next.next;
-            ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
-            ptr_old_list = ptr_old_list.next;
-            ptr_new_list = ptr_new_list.next;
-        }
-        return head_old;
-    }
-    public Node copyRandomList2(Node pHead) {
-        if (pHead == null)
-            return pHead;
-        Node cur = pHead;
-        while (cur!=null){
-            Node copyNode = new Node();
-            copyNode.val = cur.val;
-            copyNode.next = cur.next;
-            cur.next = copyNode;
-            cur = copyNode.next;
-        }
-        cur = pHead;
-        Node copyCur  = null;
-        while (cur!=null){
-            copyCur = cur.next;
-            if (cur.random !=null)
-                copyCur.random = cur.random.next;
-            cur = copyCur.next;
-        }
-        cur = pHead;
-        Node copyHead  = pHead.next;
-        copyCur  = pHead.next;
-        Node nextCur  = copyCur.next;
-        while (nextCur!=null){
-            copyCur.next =nextCur.next;
-            cur.next = nextCur;
-            cur = nextCur;
-            copyCur = cur.next;
-            nextCur = copyCur.next;
-        }
-        return copyHead;
+        return r2;
     }
 }
 // Definition for a Node.
-class Node {
-    public int val;
-    public Node next;
-    public Node random;
 
-    public Node() {}
-
-    public Node(int _val,Node _next,Node _random) {
-        val = _val;
-        next = _next;
-        random = _random;
-    }
-};
