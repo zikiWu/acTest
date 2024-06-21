c34fff844b902ac5786455c9c074396cc9e00aed
wuzekun <wzk792676244#>
2024-06-21 15:29:36 +0800

    ttt

--- src/leetCode/Solution21.java
+++ src/leetCode/Solution21.java
@@ -20,46 +20,23 @@ public class Solution21 {
         ReentrantLock reentrantLock = new ReentrantLock();
     }
 
-
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null){
             return null;
         }
-        if (l1 == null){
-            return l2;
+        if (list1 == null){
+            return list2;
         }
-        if (l2 == null){
-            return l1;
+        if (list2 == null){
+            return list1;
         }
-        ListNode  node;
-        if (l1.val < l2.val){
-             node = new ListNode(l1.val);
-            node.next = mergeTwoLists(l1.next, l2);
+        if (list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }else {
-             node = new ListNode(l2.val);
-            node.next = mergeTwoLists(l1, l2.next);
-        }
-        return node;
-    }
-
-
-    class Solution {
-        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if (l1 == null) {
-                return l2;
-            }
-            else if (l2 == null) {
-                return l1;
-            }
-            else if (l1.val < l2.val) {
-                l1.next = mergeTwoLists(l1.next, l2);
-                return l1;
-            }
-            else {
-                l2.next = mergeTwoLists(l1, l2.next);
-                return l2;
-            }
-
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
     }
+
 }
